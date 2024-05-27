d650963d0fe9f493fec730a727c2f65d7b0ae5bb
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2024-05-27 23:18:32 +0800

    Modified Files

--- vlm/src/VLMManager.py
+++ vlm/src/VLMManager.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from transformers import pipeline
 
 class VLMManager:
     def __init__(self):
@@ -8,4 +8,17 @@ class VLMManager:
 
     def identify(self, image: bytes, caption: str) -> List[int]:
         # perform object detection with a vision-language model
-        return [0, 0, 0, 0]
+
+        model_path = "google/owlvit-base-patch32"
+        detector = pipeline(model=model_path, task="zero-shot-object-detection") #model_path
+
+        predictions = detector(
+            image,
+            candidate_labels=[caption],
+            threshold=0.10,
+            top_k=3
+        )
+        
+        xmin, ymin, xmax, ymax = max(predictions, key=lambda x: x['score'])
+
+        return [xmin, ymin, xmax - xmin, ymax - ymin]
